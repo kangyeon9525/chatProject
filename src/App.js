f64ad225af7542430d8ce6236a9fe193e0b0f626
pjk3665@naver.com <pjk3665@naver.com>
2024-05-25 17:26:16 +0900

    Add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-// src/main/frontend/src/App.js
-
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import './App.css';
+import { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
-   const [hello, setHello] = useState('')
-g
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
+  const [data, setData] = useState([]);
 
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-    );
+  useEffect(() => {
+    fetch("/chat")
+      .then((res) => {
+        if (res.redirected) {
+          window.location.href = res.url; // 리다이렉트된 URL로 이동
+        } else {
+          return res.json();
+        }
+      })
+      .then(function (result) {
+        if (result) {
+          setData(result);
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ul>
+          {data.map((v, idx) => <li key={`${idx}-${v}`}>{v}</li>)}
+        </ul>
+      </header>
+    </div>
+  );
 }
 
 export default App;
